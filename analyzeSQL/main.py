a9e3f667f79be71cc8c6793a692bda809c536b3a
fuyjcnbr <paall@rambler.ru>
2021-07-31 12:01:03 +0300

    added SqlParser in main

--- analyzeSQL/main.py
+++ analyzeSQL/main.py
@@ -3,7 +3,27 @@ import lark
 from lark import Lark, Transformer, v_args, Tree, Token
 
 
-sql=""
+class SqlParser:
+	available_sql_grammars = {"simple_sql"}
+
+	def get_parser(self, sql_grammar_type: str):
+		if sql_grammar_type not in self.available_sql_grammars:
+			raise Exception(f"unknown grammar {sql_grammar_type}, should be one of: {self.available_sql_grammars}")
+
+		file_name = sql_grammar_type + ".lark"
+		with open(file_name, 'r') as file:
+			grammar = file.read()
+
+		sql_parser = Lark(grammar, parser="lalr")
+		return sql_parser
+
+
+if __name__ == "__main__":
+	sql_parser = SqlParser().get_parser("simple_sql")
+	print(f"sql_parser = {sql_parser}")
+
+
+
 
 
 
